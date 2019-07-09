0b147c8b3332e8963a99cf7fc5f85858625788ce
Federico SEBASTE <federico.sebaste@nexans.com>
2019-07-09 12:24:34 +0200

    twit bot first test

---
+++ artistis/artists.js
@@ -0,0 +1,27 @@
+const artists = [
+  "andy-warhol",
+  "pablo-picasso",
+  "banksy",
+  "keith-haring",
+  "takashi-murakami",
+  "roy-lichtenstein",
+  "damien-hirst",
+  "francis-bacon",
+  "ai-weiwei",
+  "leonardo-da-vinci",
+  "vincent-van-gogh",
+  "rembrandt-van-rijn",
+  "paolo-uccello",
+  "paul-cezanne",
+  "wassily-kandinsky",
+  "claude-monet",
+  "paul-gauguin",
+  "vincent-van-gogh",
+  "edouard-manet",
+  "edvard-munch",
+  "piero-della-francesca",
+  "masaccio",
+  "Gustave Courbet"
+];
+
+module.exports = artists;
---
+++ configDev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: "faTuC9hQ8lgTwMUh7dCyVmwQB",
+  consumer_secret: "U3W6Vq0KHnC8BDK2HRMftZvfikHJRUSQ1U5XAWGBASZl3laUqQ",
+  access_token: "1147107023907106817-Zvbb8T8rpzHw5znO4SaLz8f3fJT60c",
+  access_token_secret: "LUE4zfZMktvIEGh8SXnm65a8bTfPV2pmwlNHGUUVGzMJw"
+};
