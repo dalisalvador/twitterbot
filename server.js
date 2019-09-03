53679bbf48171f4125bf9b44c9d84b1714d8a067
F <f@MBP-de-F.lan>
2019-09-03 22:38:42 +0200

    keep alive extended to 10 min

--- server.js
+++ server.js
@@ -877,5 +877,5 @@ async function getArtist(id) {
 function keepAwake() {
   setInterval(function() {
     axios.get("http://daliweb.herokuapp.com");
-  }, 1000 * 10); // every 5 minutes (300000)
+  }, 1000 * 60 * 10); // every 5 minutes (300000)
 }
